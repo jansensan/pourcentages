85e1deaa8783d074010a711df4a016117ccf62cf
Mat Janson Blanchet <mat.jb@jansensan.net>
2024-03-15 10:10:39 -0400

    Added comments

--- public/script.js
+++ public/script.js
@@ -21,13 +21,14 @@ init();
 
 // methods definitions
 function init() {
-  // get dom elements
+  // dom elements - form
   form = document.getElementById('form');
   personalRecordInput = document.getElementById('personalRecord');
   barWeightInput = document.getElementById('barWeight');
   percentageInput = document.getElementById('percentage');
   submitButton = document.getElementById('submitButton');
 
+  // dom elements - results section
   results = document.getElementById('results');
   percentageVal = document.getElementById('percentageVal');
   weightVal = document.getElementById('weightVal');
@@ -35,6 +36,7 @@ function init() {
   totalWeightVal = document.getElementById('totalWeightVal');
   reinitButton = document.getElementById('reinitButton');
 
+  // add event listeners
   form.addEventListener('submit', onFormSubmit);
   reinitButton.addEventListener('click', onReinit);
 }
@@ -44,25 +46,31 @@ function onFormSubmit(event) {
 
   // TODO: validation
 
+  // get values from input fields
   const personalRecord = parseFloat(personalRecordInput.value);
   const barWeight = parseFloat(barWeightInput.value);
   const percentage = parseFloat(percentageInput.value);
 
+  // derive values from fields
   const prAtPercentage = personalRecord * (percentage / 100);
   const prAtPercMinusBar = prAtPercentage - barWeight;
   const weightPerSide = prAtPercMinusBar / 2;
 
+  // update results section
   percentageVal.innerText = `${ percentage }%`;
   weightVal.innerText = personalRecord.toFixed(2);
   weightPerSideVal.innerText = weightPerSide.toFixed(2);
   totalWeightVal.innerText = prAtPercentage.toFixed(2);
 
+  // show results section
   results.classList.add('visible');
 }
 
 function onReinit() {
+  // hide results section
   results.classList.remove('visible');
 
+  // reset values
   barWeightInput.value = 45;
   percentageInput.value = 100;
 }
